d4c6007deefef63901a0529b3bc0379b2d4a198f
Fernando Linhares <fernandolinhares1920@globo.com>
2021-03-13 19:48:08 -0300

    fix: changing chromatic yml

--- src/presentation/pages/home/app.tsx
+++ src/presentation/pages/home/app.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container } from '@material-ui/core'
 // Components
 import { Header, StarterContent } from '../../components'
-// App comp
+
 const App: React.FC = () => {
   return (
     <Container maxWidth="lg">
