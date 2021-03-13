e87cc256facdecf0ecdc73d39ef8c2fe19f8ad24
Fernando Linhares <fernandolinhares1920@globo.com>
2021-03-13 13:41:33 -0300

    chore: installing ts-loader, types-node and setting up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
