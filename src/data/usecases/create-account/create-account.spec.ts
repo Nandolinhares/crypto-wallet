3a26434a4bb752c0b3d49b5cacb88abff54ebd4f
Fernando Linhares <fernandolinhares1920@globo.com>
2021-03-20 18:18:42 -0300

    refactor: creating sut

--- src/data/usecases/create-account/create-account.spec.ts
+++ src/data/usecases/create-account/create-account.spec.ts
@@ -3,10 +3,24 @@ import { SetStorageMock } from '../../test/create-account/mock-create-account'
 // Faker
 import faker from 'faker'
 
+type SutTypes = {
+  sut: CreateAccount
+  setStorageMock: SetStorageMock
+}
+
+// O que vai ser testado
+const makeSut = (): SutTypes => {
+  const setStorageMock = new SetStorageMock()
+  const sut = new CreateAccount(setStorageMock)
+  return {
+    sut,
+    setStorageMock
+  }
+}
+
 describe('Create Account', () => {
   test('Should call SetStorage with correct username', () => {
-    const setStorageMock = new SetStorageMock()
-    const sut = new CreateAccount(setStorageMock)
+    const { sut, setStorageMock } = makeSut()
     const username = faker.internet.userName()
     sut.create(username)
     expect(setStorageMock.username).toBe(username)
