0bd6c57b6938de04231e7c150ff222cd9d4b4639
Fernando Linhares <fernandolinhares1920@globo.com>
2021-03-20 20:24:54 -0300

    refactor: deleting prepare checkout spy

--- src/data/test/prepare-checkout/mock-prepare-checkout.ts
+++ src/data/test/prepare-checkout/mock-prepare-checkout.ts
@@ -1,5 +1,4 @@
-import { CheckoutModel } from '../../../domain/models/checkout-model/checkout-model.type'
-import { CheckoutInterface, CheckoutParams } from '../../../domain/usecases/checkout/checkout.interface'
+import { CheckoutParams } from '../../../domain/usecases/checkout/checkout.interface'
 
 export const mockCheckoutParams = (): CheckoutParams => {
   return {
@@ -13,32 +12,6 @@ export const mockCheckoutParams = (): CheckoutParams => {
       money: '1',
       statement: [],
       username: 'teste'
-    }
-  }
-}
-
-export class PrepareCheckoutSpy implements CheckoutInterface {
-  finish (params: CheckoutParams): CheckoutModel {
-    if (parseFloat(params.user.money) > 0 && parseFloat(params.qtdValue) > 0) {
-      return {
-        error: false,
-        message: 'Ok'
-      }
-      // switch (true) {
-      //   // Caso seja compra, passa o operador de subtração do dinheiro
-      //   case (params.stateSelected === 0):
-      //     this.checkoutInformation = CalculateCheckout(params, '-')
-      //     return this.checkoutInformation
-      //   // Caso seja venda, passao o operador de adição do dinheiro
-      //   case (params.stateSelected === 1):
-      //     this.checkoutInformation = CalculateCheckout(params, '+')
-      //     return this.checkoutInformation
-      // }
-    } else {
-      return {
-        error: true,
-        message: 'Você não pode inserir valores negativos'
-      }
     }
   }
 }
